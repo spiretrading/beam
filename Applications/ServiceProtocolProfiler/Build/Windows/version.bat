27bad157e9eba7620a04ba5041c254d2674da9be
Kamal <kamal@eidolonsystems.com>
2016-08-27 19:43:58 -0400

    Fixed version script for ServiceProtocolProfiler.

--- Applications/ServiceProtocolProfiler/Build/Windows/version.bat
+++ Applications/ServiceProtocolProfiler/Build/Windows/version.bat
@@ -5,4 +5,5 @@ mkdir ServiceProtocolProfiler
 cd %~dp0
 printf "#define SERVICE_PROTOCOL_PROFILER_VERSION """> %~dp0../../Include/ServiceProtocolProfiler/Version.hpp
 git rev-list --count --first-parent HEAD | tr -d "\n\" >> %~dp0../../Include/ServiceProtocolProfiler/Version.hpp
+printf """" >> %~dp0../../Include/ServiceProtocolProfiler/Version.hpp
 printf "\n" >> %~dp0../../Include/ServiceProtocolProfiler/Version.hpp

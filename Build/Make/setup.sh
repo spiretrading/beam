cfea9290bea2dda654f96fcbc3d9127793673455
Kamal <kamal@eidolonsystems.com>
2017-01-19 12:01:16 -0500

    The setup script only uses sudo permissions for necessary operations.

--- Build/Make/setup.sh
+++ Build/Make/setup.sh
@@ -2,42 +2,42 @@
 let cores="`grep -c "processor" < /proc/cpuinfo`"
 
 if [ ! -d "cppunit-1.12.1" ]; then
-  wget http://downloads.sourceforge.net/cppunit/cppunit-1.12.1.tar.gz
+  sudo -u $(logname) wget http://downloads.sourceforge.net/cppunit/cppunit-1.12.1.tar.gz
   if [ -f cppunit-1.12.1.tar.gz ]; then
-    gzip -d -c cppunit-1.12.1.tar.gz | tar -x
+    sudo -u $(logname) gzip -d -c cppunit-1.12.1.tar.gz | tar -x
     cd cppunit-1.12.1
-    cat configure | sed "s/\/\* automatically generated \*\//\$ac_prefix_conf_INP/" > configure.new
-    mv configure.new configure
-    chmod +rwx configure
-    ./configure LDFLAGS='-ldl'
-    make -j $cores
+    sudo -u $(logname) cat configure | sed "s/\/\* automatically generated \*\//\$ac_prefix_conf_INP/" > configure.new
+    sudo -u $(logname) mv configure.new configure
+    sudo -u $(logname) chmod +rwx configure
+    sudo -u $(logname) ./configure LDFLAGS='-ldl'
+    sudo -u $(logname) make -j $cores
     make install
     cd ..
     rm -f cppunit-1.12.1.tar.gz
   fi
 fi
 if [ ! -d "cryptopp565" ]; then
-  wget http://www.cryptopp.com/cryptopp565.zip
+  sudo -u $(logname) wget http://www.cryptopp.com/cryptopp565.zip
   if [ -f cryptopp565.zip ]; then
-    mkdir cryptopp565
+    sudo -u $(logname) mkdir cryptopp565
     cd cryptopp565
-    unzip ../cryptopp565.zip
-    cat GNUmakefile | sed "s/# CXXFLAGS += -fPIC/CXXFLAGS += -fPIC/" > GNUmakefile.new
-    mv GNUmakefile.new GNUmakefile
-    make -j $cores
+    sudo -u $(logname) unzip ../cryptopp565.zip
+    sudo -u $(logname) cat GNUmakefile | sed "s/# CXXFLAGS += -fPIC/CXXFLAGS += -fPIC/" > GNUmakefile.new
+    sudo -u $(logname) mv GNUmakefile.new GNUmakefile
+    sudo -u $(logname) make -j $cores
     make install
     cd ..
     rm -f cryptopp565.zip
   fi
 fi
 if [ ! -d "zlib-1.2.11" ]; then
-  wget http://www.zlib.net/zlib-1.2.11.tar.gz
+  sudo -u $(logname) wget http://www.zlib.net/zlib-1.2.11.tar.gz
   if [ -f zlib-1.2.11.tar.gz ]; then
-    gzip -d -c zlib-1.2.11.tar.gz | tar -x
+    sudo -u $(logname) gzip -d -c zlib-1.2.11.tar.gz | tar -x
     cd zlib-1.2.11
     export CFLAGS="-fPIC"
-    cmake -G "Unix Makefiles" -DAMD64=ON
-    make -j $cores
+    sudo -u $(logname) cmake -G "Unix Makefiles" -DAMD64=ON
+    sudo -u $(logname) make -j $cores
     make install
     unset CFLAGS
     cd ..
@@ -45,47 +45,47 @@ if [ ! -d "zlib-1.2.11" ]; then
   fi
 fi
 if [ ! -d "mysql-connector-c-6.1.6" ]; then
-  wget http://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.6-src.tar.gz
+  sudo -u $(logname) wget http://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.6-src.tar.gz
   if [ -f mysql-connector-c-6.1.6-src.tar.gz ]; then
-    gzip -d -c mysql-connector-c-6.1.6-src.tar.gz | tar -x
-    mv mysql-connector-c-6.1.6-src mysql-connector-c-6.1.6
+    sudo -u $(logname) gzip -d -c mysql-connector-c-6.1.6-src.tar.gz | tar -x
+    sudo -u $(logname) mv mysql-connector-c-6.1.6-src mysql-connector-c-6.1.6
     cd mysql-connector-c-6.1.6
-    cmake -G "Unix Makefiles"
-    make -j $cores
+    sudo -u $(logname) cmake -G "Unix Makefiles"
+    sudo -u $(logname) make -j $cores
     make install
     cd ..
     rm -f mysql-connector-c-6.1.6-src.tar.gz
   fi
 fi
 if [ ! -d "mysql++-3.2.2" ]; then
-  wget http://tangentsoft.net/mysql++/releases/mysql++-3.2.2.tar.gz
+  sudo -u $(logname) wget http://tangentsoft.net/mysql++/releases/mysql++-3.2.2.tar.gz
   if [ -f mysql++-3.2.2.tar.gz ]; then
-    gzip -d -c mysql++-3.2.2.tar.gz | tar -x
+    sudo -u $(logname) gzip -d -c mysql++-3.2.2.tar.gz | tar -x
     cd mysql++-3.2.2
-    ./configure
-    make -j $cores
+    sudo -u $(logname) ./configure
+    sudo -u $(logname) make -j $cores
     make install
     cd ..
     rm -f mysql++-3.2.2.tar.gz
   fi
 fi
 if [ ! -d "yaml-cpp" ]; then
-  wget https://github.com/jbeder/yaml-cpp/archive/release-0.2.7.zip
+  sudo -u $(logname) wget https://github.com/jbeder/yaml-cpp/archive/release-0.2.7.zip
   if [ -f release-0.2.7.zip ]; then
-    unzip release-0.2.7.zip
-    mv yaml-cpp-release-0.2.7 yaml-cpp
+    sudo -u $(logname) unzip release-0.2.7.zip
+    sudo -u $(logname) mv yaml-cpp-release-0.2.7 yaml-cpp
     cd yaml-cpp/include/yaml-cpp
-    head -7 noncopyable.h > noncopyable.h.new
-    printf "#include <stdlib.h>" >> noncopyable.h.new
-    tail -n+7 noncopyable.h >> noncopyable.h.new
-    mv noncopyable.h.new noncopyable.h
+    sudo -u $(logname) head -7 noncopyable.h > noncopyable.h.new
+    sudo -u $(logname) printf "#include <stdlib.h>" >> noncopyable.h.new
+    sudo -u $(logname) tail -n+7 noncopyable.h >> noncopyable.h.new
+    sudo -u $(logname) mv noncopyable.h.new noncopyable.h
     cd ../../
-    mkdir build
+    sudo -u $(logname) mkdir build
     cd build
     export CFLAGS="-fPIC"
     export CXXFLAGS="-fPIC"
-    cmake ..
-    make -j $cores
+    sudo -u $(logname) cmake ..
+    sudo -u $(logname) make -j $cores
     make install
     unset CFLAGS
     unset CXXFLAGS
@@ -94,26 +94,26 @@ if [ ! -d "yaml-cpp" ]; then
   fi
 fi
 if [ ! -d "tclap-1.2.1" ]; then
-  wget "https://downloads.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz?r=&ts=1309913922&use_mirror=superb-sea2" -O tclap-1.2.1.tar.gz --no-check-certificate
+  sudo -u $(logname) wget "https://downloads.sourceforge.net/project/tclap/tclap-1.2.1.tar.gz?r=&ts=1309913922&use_mirror=superb-sea2" -O tclap-1.2.1.tar.gz --no-check-certificate
   if [ -f tclap-1.2.1.tar.gz ]; then
-    gzip -d -c tclap-1.2.1.tar.gz | tar -x
+    sudo -u $(logname) gzip -d -c tclap-1.2.1.tar.gz | tar -x
     cd tclap-1.2.1
-    ./configure
-    make -j $cores
+    sudo -u $(logname) ./configure
+    sudo -u $(logname) make -j $cores
     make install
     cd ..
     rm -f tclap-1.2.1.tar.gz
   fi
 fi
 if [ ! -d "openssl-1.0.2g" ]; then
-  wget --no-check-certificate https://www.openssl.org/source/openssl-1.0.2g.tar.gz
+  sudo -u $(logname) wget --no-check-certificate https://www.openssl.org/source/openssl-1.0.2g.tar.gz
   if [ -f openssl-1.0.2g.tar.gz ]; then
-    gzip -d -c openssl-1.0.2g.tar.gz | tar -x
+    sudo -u $(logname) gzip -d -c openssl-1.0.2g.tar.gz | tar -x
     cd openssl-1.0.2g
     export LDFLAGS=-ldl
-    ./config no-shared threads -fPIC -ldl
-    make
-    make test
+    sudo -u $(logname) ./config no-shared threads -fPIC -ldl
+    sudo -u $(logname) make
+    sudo -u $(logname) make test
     make install
     unset LDFLAGS
     cd ..
@@ -121,22 +121,22 @@ if [ ! -d "openssl-1.0.2g" ]; then
   fi
 fi
 if [ ! -d "boost_1_61_0" ]; then
-  wget http://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download -O boost_1_61_0.tar.gz
+  sudo -u $(logname) wget http://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download -O boost_1_61_0.tar.gz
   if [ -f boost_1_61_0.tar.gz ]; then
-    tar xvf boost_1_61_0.tar.gz
+    sudo -u $(logname) tar xvf boost_1_61_0.tar.gz
     cd boost_1_61_0
-    ./bootstrap.sh
+    sudo -u $(logname) ./bootstrap.sh
     ./bjam -j$cores cxxflags="-std=c++14 -fPIC" install
     cd ..
     rm -f boost_1_61_0.tar.gz
   fi
 fi
 if [ ! -d "lua-5.3.1" ]; then
-  wget http://www.lua.org/ftp/lua-5.3.1.tar.gz
+  sudo -u $(logname) wget http://www.lua.org/ftp/lua-5.3.1.tar.gz
   if [ -f lua-5.3.1.tar.gz ]; then
-    gzip -d -c lua-5.3.1.tar.gz | tar -x
+    sudo -u $(logname) gzip -d -c lua-5.3.1.tar.gz | tar -x
     cd lua-5.3.1
-    make -j $cores linux
+    sudo -u $(logname) make -j $cores linux
     make linux install
     cd ..
     rm -f lua-5.3.1.tar.gz

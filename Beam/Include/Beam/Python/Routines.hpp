94f2b18ef4bac55bd00033fbb96d43119c6d49fc
Kamal <kamal@eidolonsystems.com>
2017-02-24 18:46:21 -0500

    Added Python bindings for Routines namespace.

--- Beam/Include/Beam/Python/Routines.hpp
+++ Beam/Include/Beam/Python/Routines.hpp
@@ -1,9 +1,30 @@
 #ifndef BEAM_PYTHONROUTINES_HPP
 #define BEAM_PYTHONROUTINES_HPP
 #include "Beam/Python/Python.hpp"
+#include "Beam/Routines/Async.hpp"
 
 namespace Beam {
 namespace Python {
+
+  //! Exports the Async class.
+  template<typename T>
+  void ExportAsync(const char* name) {
+    boost::python::class_<Routines::Async<T>, boost::noncopyable,
+      boost::python::bases<Routines::BaseAsync>>(name)
+      .def("get_eval", &Async<T>::GetEval)
+      .def("get", BlockingFunction(&Routines::Async<T>::Get,
+        boost::python::return_value_policy<
+        boost::python::copy_non_const_reference>()))
+      .def("get_exception", &Async<T>::GetException)
+      .add_property("state", &Routines::Async<T>::GetState)
+      .def("reset", &Routines::Async<T>::Reset);
+  }
+
+  //! Exports the RoutineHandler class.
+  void ExportRoutineHandler();
+
+  //! Exports the RoutineHandlerGroup class.
+  void ExportRoutineHandlerGroup();
 
   //! Exports the Routines namespace.
   void ExportRoutines();
--- Beam/Source/Python/Routines.cpp
+++ Beam/Source/Python/Routines.cpp
@@ -1,7 +1,9 @@
 #include "Beam/Python/Routines.hpp"
 #include "Beam/Python/BoostPython.hpp"
+#include "Beam/Python/GilLock.hpp"
 #include "Beam/Python/GilRelease.hpp"
 #include "Beam/Routines/RoutineHandler.hpp"
+#include "Beam/Routines/RoutineHandlerGroup.hpp"
 
 using namespace Beam;
 using namespace Beam::Python;
@@ -10,11 +12,47 @@ using namespace boost;
 using namespace boost::python;
 using namespace std;
 
+namespace {
+  void RoutineHandlerGroupAddRoutineHandler(RoutineHandlerGroup& routines,
+      RoutineHandler* handler) {
+    routines.Add(std::move(*handler));
+  }
+
+  void RoutineHandlerGroupSpawn(RoutineHandlerGroup& routines,
+      object object) {
+    routines.Spawn(
+      [=] {
+        GilLock gil;
+        boost::lock_guard<GilLock> lock{gil};
+        object();
+      });
+  }
+}
+
+void Beam::Python::ExportRoutineHandler() {
+  class_<RoutineHandler, noncopyable>("RoutineHandler", init<>())
+    .def(init<Routine::Id>())
+    .add_property("id", &RoutineHandler::GetId)
+    .def("detach", &RoutineHandler::Detach)
+    .def("wait", BlockingFunction(&RoutineHandler::Wait));
+  def("flush_pending_routines", BlockingFunction(&FlushPendingRoutines));
+}
+
+void Beam::Python::ExportRoutineHandlerGroup() {
+  class_<RoutineHandlerGroup, noncopyable>("RoutineHandlerGroup", init<>())
+    .def("add", static_cast<void (RoutineHandlerGroup::*)(Routine::Id)>(
+      &RoutineHandlerGroup::Add))
+    .def("add", &RoutineHandlerGroupAddRoutineHandler)
+    .def("spawn", &RoutineHandlerGroupSpawn)
+    .def("wait", BlockingFunction(&RoutineHandlerGroup::Wait));
+}
+
 void Beam::Python::ExportRoutines() {
   string nestedName = extract<string>(scope().attr("__name__") + ".routines");
   object nestedModule{handle<>(
     borrowed(PyImport_AddModule(nestedName.c_str())))};
   scope().attr("routines") = nestedModule;
   scope parent = nestedModule;
-  def("flush_pending_routines", BlockingFunction(&FlushPendingRoutines));
+  ExportRoutineHandler();
+  ExportRoutineHandlerGroup();
 }

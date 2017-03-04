9b1dbb6889c3569cc56e22fdbc70f562577fc610
Kamal <kamal@eidolonsystems.com>
2017-03-04 15:10:42 -0500

    .

--- Beam/Include/Beam/WebServices/HttpServletContainer.hpp
+++ Beam/Include/Beam/WebServices/HttpServletContainer.hpp
@@ -3,11 +3,44 @@
 #include <boost/noncopyable.hpp>
 #include "Beam/Pointers/Dereference.hpp"
 #include "Beam/Pointers/LocalPtr.hpp"
+#include "Beam/Utilities/StaticMemberChecks.hpp"
 #include "Beam/WebServices/HttpServer.hpp"
 #include "Beam/WebServices/WebServices.hpp"
 
 namespace Beam {
 namespace WebServices {
+namespace Details {
+  BEAM_DEFINE_HAS_METHOD(IsServletClass, Open, void);
+  BEAM_DEFINE_HAS_METHOD(HasWebSocketSlots, GetWebSocketSlots, void);
+  template<typename ContainerType, typename ServletType,
+    bool dummy = IsServletClass<ServletType>::value>
+  struct GetServletHelper;
+
+  template<typename ContainerType, typename ServletType>
+  struct GetServletHelper<ContainerType, ServletType, true> {
+    using type = ServletType;
+  };
+
+  template<typename ContainerType, typename ServletType>
+  struct GetServletHelper<ContainerType, ServletType, false> {
+    using type = typename ServletType::template apply<ContainerType>::type;
+  };
+
+  template<typename ContainerType, typename ServletType>
+  using GetServlet = typename GetServletHelper<
+    ContainerType, ServletType>::type;
+
+  template<typename Container, typename Servlet>
+  auto GetWebSocketSlots(Servlet& servlet) ->
+      decltype(servlet.GetWebSocketSlots()) {
+    return servlet.GetWebSocketSlots();
+  }
+
+  template<typename Container, typename Servlet>
+  auto GetWebSocketSlots(Servlet& servlet) {
+    return std::vector<Container::HttpServer::HttpUpgradeSlot>();
+  }
+}
 
   /*! \class HttpServletContainer
       \brief Composes an HttpServlet with an HttpServer.
@@ -20,10 +53,13 @@ namespace WebServices {
     public:
 
       //! The type of HttpServlet servicing HTTP requests.
-      using Servlet = ServletType;
+      using Servlet = Details::GetServlet<HttpServletContainer, ServletType>;
 
       //! The type of ServerConnection accepting Channels.
       using ServerConnection = GetTryDereferenceType<ServerConnectionType>;
+
+      //! The type of HttpServer used.
+      using HttpServer = Beam::WebServices::HttpServer<ServerConnectionType>;
 
       //! Constructs the HttpServletContainer.
       /*!
@@ -42,7 +78,7 @@ namespace WebServices {
 
     private:
       Beam::LocalPtr<Servlet> m_servlet;
-      Beam::LocalPtr<HttpServer<ServerConnectionType>> m_server;
+      Beam::LocalPtr<HttpServer> m_server;
   };
 
   template<typename ServletType, typename ServerConnectionType>
@@ -51,7 +87,8 @@ namespace WebServices {
       ServletForward&& servlet, ServerConnectionForward&& serverConnection)
       : m_servlet{std::forward<ServletForward>(servlet)},
         m_server{std::forward<ServerConnectionForward>(serverConnection),
-          m_servlet->GetSlots()} {}
+          m_servlet->GetSlots(),
+          Details::GetWebSocketSlots<HttpServletContainer>(*m_servlet)} {}
 
   template<typename ServletType, typename ServerConnectionType>
   HttpServletContainer<ServletType, ServerConnectionType>::

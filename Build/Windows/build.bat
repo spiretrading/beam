b62579f7321bd9e16086c0cabeb861fe8563698b
Kamal <kamal@eidolonsystems.com>
2017-03-18 21:14:55 -0400

    Fixed various linker warnings.

--- Build/Windows/build.bat
+++ Build/Windows/build.bat
@@ -13,6 +13,7 @@ CALL %~dp0../../Applications/QueryStressTest/Build/Windows/build.bat %config%
 CALL %~dp0../../Applications/RegistryServer/Build/Windows/build.bat %config%
 CALL %~dp0../../Applications/ServiceLocator/Build/Windows/build.bat %config%
 CALL %~dp0../../Applications/ServiceProtocolProfiler/Build/Windows/build.bat %config%
+CALL %~dp0../../Applications/ServletTemplate/Build/Windows/build.bat %config%
 CALL %~dp0../../Applications/UidServer/Build/Windows/build.bat %config%
 CALL %~dp0../../Applications/WebSocketEchoServer/Build/Windows/build.bat %config%
 ENDLOCAL

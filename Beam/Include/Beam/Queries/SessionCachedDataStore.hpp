3f9f7134ec39b277c2f8e8614690df0589241adf
Kamal <kamal@eidolonsystems.com>
2017-03-31 14:55:59 -0400

    Revert "SessionCachedDataStore will initialize its cache with the last value stored in the database."
    This reverts commit 0d4bfa1b27b78daa733ac67fd7a4675724a9a50c.
    
    # Conflicts:
    #	Beam/Include/Beam/Queries/SessionCachedDataStore.hpp

--- Beam/Include/Beam/Queries/SessionCachedDataStore.hpp
+++ Beam/Include/Beam/Queries/SessionCachedDataStore.hpp
@@ -94,20 +94,7 @@ namespace Queries {
   template<typename DataStoreType, typename EvaluatorTranslatorFilterType>
   Sequence SessionCachedDataStore<DataStoreType,
       EvaluatorTranslatorFilterType>::LoadInitialSequence(const Index& index) {
-    Query query;
-    query.SetIndex(index);
-    query.SetRange(Range::Total());
-    query.SetSnapshotLimit(SnapshotLimit::Type::TAIL, 1);
-    auto& cache = LoadCache(index);
-    auto result = m_dataStore->Load(query);
-    if(result.empty()) {
-      cache.Initialize(boost::posix_time::neg_infin, Sequence::First());
-      return Sequence{1};
-    }
-    cache.Store(MakeSequencedValue(MakeIndexedValue(
-      std::move(result.back().GetValue()), index),
-      result.back().GetSequence()));
-    return Increment(result.back().GetSequence());
+    return m_dataStore->LoadInitialSequence(index);
   }
 
   template<typename DataStoreType, typename EvaluatorTranslatorFilterType>

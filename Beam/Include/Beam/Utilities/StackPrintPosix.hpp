d0e1a362f9ec68554a4e5f2ae8fbe0dfc554cde2
Kamal <kamal@eidolonsystems.com>
2017-06-14 11:15:44 -0400

    Removed libunwind.

--- Beam/Include/Beam/Utilities/StackPrintPosix.hpp
+++ Beam/Include/Beam/Utilities/StackPrintPosix.hpp
@@ -1,52 +1,30 @@
 #ifndef BEAM_STACKPRINTPOSIX_HPP
 #define BEAM_STACKPRINTPOSIX_HPP
-#ifdef BEAM_ENABLE_STACK_PRINT
-#include <cxxabi.h>
-#include <libunwind.h>
 #include <cstdio>
 #include <cstdlib>
-#endif
 #include <string>
+#include <cxxabi.h>
+#include <execinfo.h>
 #include "Beam/Utilities/Utilities.hpp"
 
 namespace Beam {
   inline std::string CaptureStackPrint() {
-#ifdef BEAM_ENABLE_STACK_PRINT
-    unw_cursor_t cursor;
-    unw_context_t context;
-    unw_getcontext(&context);
-    unw_init_local(&cursor, &context);
-    char buffer[1024];
     std::string result;
-    while(unw_step(&cursor) > 0) {
-      unw_word_t offset;
-      unw_word_t pc;
-      unw_get_reg(&cursor, UNW_REG_IP, &pc);
-      if(pc == 0) {
-        break;
-      }
-      std::snprintf(buffer, sizeof(buffer), "0x%lx:", pc);
+#ifdef BEAM_ENABLE_STACK_PRINT
+#ifdef NDEBUG
+    const auto MAX_DEPTH = 100;
+    void* stackAddresses[MAX_DEPTH];
+    auto stackDepth = backtrace(stackAddresses, MAX_DEPTH);
+    auto stackStrings = backtrace_symbols(stackAddresses, stackDepth);
+    char buffer[1024];
+    for(auto i = 1; i < stackDepth; ++i) {
+      std::snprintf(buffer, sizeof(buffer), "%s\n", stackStrings[i]);
       result += buffer;
-      char sym[256];
-      if(unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0) {
-        char* nameptr = sym;
-        int status;
-        char* demangled = abi::__cxa_demangle(sym, nullptr, nullptr, &status);
-        if(status == 0) {
-          nameptr = demangled;
-        }
-        std::snprintf(buffer, sizeof(buffer), " (%s+0x%lx)\n", nameptr, offset);
-        result += buffer;
-        std::free(demangled);
-      } else {
-        std::snprintf(buffer, sizeof(buffer),
-          " -- error: unable to obtain symbol name for this frame\n");
-        result += buffer;
-      }
     }
-    return result;
+    std::free(stackStrings);
+#endif
 #else
-    return "";
+    return result;
 #endif
   }
 }

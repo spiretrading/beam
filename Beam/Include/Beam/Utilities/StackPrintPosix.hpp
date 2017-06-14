03c4ca60c5fb82c259f2138ba2a6dad584844eda
Kamal <kamal@eidolonsystems.com>
2017-06-14 11:33:34 -0400

    Removed stack printing.

--- Beam/Include/Beam/Utilities/StackPrintPosix.hpp
+++ Beam/Include/Beam/Utilities/StackPrintPosix.hpp
@@ -12,16 +12,6 @@ namespace Beam {
     std::string result;
 #ifdef BEAM_ENABLE_STACK_PRINT
 #ifdef NDEBUG
-    const auto MAX_DEPTH = 100;
-    void* stackAddresses[MAX_DEPTH];
-    auto stackDepth = backtrace(stackAddresses, MAX_DEPTH);
-    auto stackStrings = backtrace_symbols(stackAddresses, stackDepth);
-    char buffer[1024];
-    for(auto i = 1; i < stackDepth; ++i) {
-      std::snprintf(buffer, sizeof(buffer), "%s\n", stackStrings[i]);
-      result += buffer;
-    }
-    std::free(stackStrings);
 #endif
 #else
     return result;

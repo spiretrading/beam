86e0be1c627c6cba53cc7628424acde4fbdea403
Kamal Mansouri <kamal@eidolonsystems.com>
2018-06-11 21:02:39 -0400

    Simplified the web api build scripts so that there is only a single build script.

---
+++ web_api/build/build.bat
@@ -0,0 +1,51 @@
+SETLOCAL
+IF NOT "%1" == "Debug" (
+  SET PROD_ENV=1
+)
+PUSHD %~dp0..\
+IF NOT EXIST node_modules (
+  SET UPDATE_NODE=1
+) ELSE (
+  PUSHD node_modules
+  IF NOT EXIST mod_time.txt (
+    SET UPDATE_NODE=1
+  ) ELSE (
+    FOR /F %%i IN (
+      'ls -l --time-style=full-iso ..\package.json ^| awk "{print $6 $7}"') DO (
+      FOR /F %%j IN (
+        'ls -l --time-style=full-iso mod_time.txt ^| awk "{print $6 $7}"') DO (
+        IF "%%i" GEQ "%%j" (
+          SET UPDATE_NODE=1
+        )
+      )
+    )
+  )
+  POPD
+)
+IF "%UPDATE_NODE%" == "1" (
+  CALL npm install
+  PUSHD node_modules
+  echo "timestamp" > mod_time.txt
+  POPD
+)
+IF NOT EXIST library (
+  SET UPDATE_LIBRARY=1
+) ELSE (
+  FOR /F %%i IN (
+    'dir source /s/b/a-d ^| tr "\\" "/" ^| xargs ls -l --time-style=full-iso ^| awk "{print $6 $7}" ^| sort /R ^| head -1') DO (
+    FOR /F %%j IN (
+      'dir library /s/b/a-d ^| tr "\\" "/" ^| xargs ls -l --time-style=full-iso ^| awk "{print $6 $7}" ^| sort /R ^| head -1') DO (
+      IF "%%i" GEQ "%%j" (
+        SET UPDATE_LIBRARY=1
+      )
+    )
+  )
+)
+IF "%UPDATE_LIBRARY%" == "1" (
+  IF EXIST library (
+    rm -rf library
+  )
+  node .\node_modules\webpack\bin\webpack.js
+)
+POPD
+ENDLOCAL
---
+++ web_api/build/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+if [ $# -eq 0 ] || [ "$1" -neq "Debug" ]; then
+  export PROD_ENV=1
+fi
+pushd ../
+if [ ! -d "node_modules" ]; then
+  UPDATE_NODE=1
+else
+  pushd node_modules
+  if [ ! -f "mod_time.txt" ]; then
+    UPDATE_NODE=1
+  else
+    pt="$(ls -l --time-style=full-iso ../package.json | awk '{print $6 $7}')"
+    mt="$(ls -l --time-style=full-iso mod_time.txt | awk '{print $6 $7}')"
+    if [ "$pt" -ge "$mt" ]; then
+      UPDATE_NODE=1
+    fi
+  fi
+  popd
+fi
+if [ "$UPDATE_NODE" -eq "1" ]; then
+  npm install
+  pushd node_modules
+  echo "timestamp" > mod_time.txt
+  popd
+fi
+if [ ! -d "library" ]; then
+  UPDATE_LIBRARY=1
+else
+  st="$(find source/ -type f | xargs ls -l --time-style=full-iso | awk "{print $6 $7}" ^| sort -r ^| head -1)"
+  lt="$(find library/ -type f | xargs ls -l --time-style=full-iso | awk "{print $6 $7}" ^| sort -r ^| head -1)"
+  if [ "$st" -ge "$lt" ]; then
+    UPDATE_LIBRARY=1
+  fi
+fi
+if [ "$UPDATE_LIBRARY" -eq "1" ]; then
+  if [ -d library ]; then
+    rm -rf library
+  fi
+  node ./node_modules/webpack/bin/webpack.js
+)
+popd

abcf3848d4ffa3cf716c037bf1ffc3318fdaa799
Kamal <kamal@eidolonsystems.com>
2018-09-19 18:09:43 -0400

    Routine ids are generated in a manner that makes them safe for use in shared objects/DLLs.

--- Beam/Include/Beam/Python/Python.hpp
+++ Beam/Include/Beam/Python/Python.hpp
@@ -12,6 +12,9 @@ BEAM_EXTERN template class BEAM_EXPORT_DLL
 BEAM_EXTERN template struct BEAM_EXPORT_DLL
   Beam::Routines::Details::CurrentRoutineGlobal<void>;
 
+BEAM_EXTERN template struct BEAM_EXPORT_DLL
+  Beam::Routines::Details::NextId<void>;
+
 namespace Beam {
 namespace Python {
 

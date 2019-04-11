bbec8935bcfd79928984b03e11cd37733151e400
Kamal <kamal@spiretrading.com>
2019-04-11 07:55:51 -0700

    Added missing filesystem dependency.

--- build.sh
+++ build.sh
@@ -6,7 +6,19 @@ while [ -h "$source" ]; do
   [[ $source != /* ]] && source="$dir/$source"
 done
 directory="$(cd -P "$(dirname "$source" )" >/dev/null 2>&1 && pwd)"
-root="$(pwd)"
+
+build_function() {
+  if [ ! -d "$1" ]; then
+    mkdir -p "$1"
+  fi
+  pushd "$1"
+  $directory/$1/build.sh "$@"
+  popd
+}
+
+export -f build_function
+export directory
+
 targets="Beam"
 targets+=" Applications/AdminClient"
 targets+=" Applications/ClientTemplate"
@@ -20,11 +32,8 @@ targets+=" Applications/ServletTemplate"
 targets+=" Applications/UidServer"
 targets+=" Applications/WebSocketEchoServer"
 
-for i in $targets; do
-  if [ ! -d "$i" ]; then
-    mkdir -p "$i"
-  fi
-  pushd "$i"
-  $directory/$i/build.sh "$@"
-  popd
-done
+let cores="`grep -c "processor" < /proc/cpuinfo` / 2 + 1"
+let mem="`grep -oP "MemTotal: +\K([[:digit:]]+)(?=.*)" < /proc/meminfo` / 4194304"
+let jobs="$(($cores<$mem?$cores:$mem))"
+
+parallel -j$jobs --no-notice build_function ::: $targets

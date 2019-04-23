94b2d55cbe9803fba31fddfed960e70023bd2fb7
Kamal Mansouri <kamal@eidolonsystems.com>
2019-04-22 22:34:40 -0400

    Renamed run_cmake.bat to configure.bat.

---
+++ configure.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+SETLOCAL
+SET ROOT=%cd%
+IF NOT EXIST configure.bat (
+  ECHO CALL "%~dp0configure.bat" %%* > configure.bat
+)
+IF NOT EXIST build.bat (
+  ECHO CALL "%~dp0build.bat" %%* > build.bat
+)
+CALL:configure Beam %*
+CALL:configure Applications\AdminClient %*
+CALL:configure Applications\ClientTemplate %*
+CALL:configure Applications\DataStoreProfiler %*
+CALL:configure Applications\HttpFileServer %*
+CALL:configure Applications\QueryStressTest %*
+CALL:configure Applications\RegistryServer %*
+CALL:configure Applications\ServiceLocator %*
+CALL:configure Applications\ServiceProtocolProfiler %*
+CALL:configure Applications\ServletTemplate %*
+CALL:configure Applications\UidServer %*
+CALL:configure Applications\WebSocketEchoServer %*
+ENDLOCAL
+EXIT /B %ERRORLEVEL%
+
+:configure
+IF NOT EXIST "%~1" (
+  MD "%~1"
+)
+PUSHD "%~1"
+IF NOT EXIST configure.bat (
+  ECHO CALL "%~dp0%~1\configure.bat" %%* > configure.bat
+)
+CALL "%~dp0%~1\configure.bat" -DD="%ROOT%\Dependencies" %~2 %~3 %~4 %~5 %~6 %~7
+POPD
+EXIT /B 0

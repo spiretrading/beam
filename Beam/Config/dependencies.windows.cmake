65e3b1c5146e7904426543a9515fd5591db83491
Kamal Mansouri <kamal@eidolonsystems.com>
2019-05-14 21:41:55 -0400

    Dependencies path can always be found in the project directory. Setup scripts no longer run as part of CMake.

--- Beam/Config/dependencies.windows.cmake
+++ Beam/Config/dependencies.windows.cmake
@@ -1,58 +1,57 @@
 set(BEAM_INCLUDE_PATH "${CMAKE_CURRENT_LIST_DIR}/../Include")
 set(BEAM_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../Source")
-set(BOOST_INCLUDE_PATH "${DEPENDENCIES_PATH}/boost_1_70_0/include/boost-1_70")
-set(BOOST_DEBUG_PATH "${DEPENDENCIES_PATH}/boost_1_70_0/lib")
-set(BOOST_OPTIMIZED_PATH "${DEPENDENCIES_PATH}/boost_1_70_0/lib")
+set(BOOST_INCLUDE_PATH "Dependencies/boost_1_70_0/include/boost-1_70")
+set(BOOST_DEBUG_PATH "Dependencies/boost_1_70_0/lib")
+set(BOOST_OPTIMIZED_PATH "Dependencies/boost_1_70_0/lib")
 set(BOOST_PYTHON_LIBRARY_DEBUG_PATH
   "${BOOST_DEBUG_PATH}/boost_python37-vc141-mt-gd-x32-1_70.lib")
 set(BOOST_PYTHON_LIBRARY_OPTIMIZED_PATH
   "${BOOST_OPTIMIZED_PATH}/boost_python37-vc141-mt-x32-1_70.lib")
-set(CPPUNIT_INCLUDE_PATH "${DEPENDENCIES_PATH}/cppunit-1.14.0/include")
-set(CPPUNIT_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/cppunit-1.14.0/lib/cppunitd.lib")
+set(CPPUNIT_INCLUDE_PATH "Dependencies/cppunit-1.14.0/include")
+set(CPPUNIT_LIBRARY_DEBUG_PATH "Dependencies/cppunit-1.14.0/lib/cppunitd.lib")
 set(CPPUNIT_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/cppunit-1.14.0/lib/cppunit.lib")
-set(CRYPTOPP_INCLUDE_PATH "${DEPENDENCIES_PATH}/cryptopp610/include")
+  "Dependencies/cppunit-1.14.0/lib/cppunit.lib")
+set(CRYPTOPP_INCLUDE_PATH "Dependencies/cryptopp610/include")
 set(CRYPTOPP_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/cryptopp610/Win32/Output/Debug/cryptlib.lib")
+  "Dependencies/cryptopp610/Win32/Output/Debug/cryptlib.lib")
 set(CRYPTOPP_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/cryptopp610/Win32/Output/Release/cryptlib.lib")
-set(LUA_INCLUDE_PATH "${DEPENDENCIES_PATH}/lua-5.3.1/src")
-set(LUA_LIBRARY_DEBUG_PATH "${DEPENDENCIES_PATH}/lua-5.3.1/src/Debug/lua.lib")
-set(LUA_LIBRARY_OPTIMIZED_PATH "${DEPENDENCIES_PATH}/lua-5.3.1/src/Release/lua.lib")
-set(MYSQL_INCLUDE_PATH "${DEPENDENCIES_PATH}/mariadb-connector-c-3.0.6/include")
+  "Dependencies/cryptopp610/Win32/Output/Release/cryptlib.lib")
+set(LUA_INCLUDE_PATH "Dependencies/lua-5.3.1/src")
+set(LUA_LIBRARY_DEBUG_PATH "Dependencies/lua-5.3.1/src/Debug/lua.lib")
+set(LUA_LIBRARY_OPTIMIZED_PATH "Dependencies/lua-5.3.1/src/Release/lua.lib")
+set(MYSQL_INCLUDE_PATH "Dependencies/mariadb-connector-c-3.0.6/include")
 set(MYSQL_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/mariadb-connector-c-3.0.6/libmariadb/Debug/mariadbclient.lib")
+  "Dependencies/mariadb-connector-c-3.0.6/libmariadb/Debug/mariadbclient.lib")
 set(MYSQL_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/mariadb-connector-c-3.0.6/libmariadb/Release/mariadbclient.lib")
-set(OPEN_SSL_INCLUDE_PATH "${DEPENDENCIES_PATH}/openssl-1.0.2g/inc32")
+  "Dependencies/mariadb-connector-c-3.0.6/libmariadb/Release/mariadbclient.lib")
+set(OPEN_SSL_INCLUDE_PATH "Dependencies/openssl-1.0.2g/inc32")
 set(OPEN_SSL_BASE_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/openssl-1.0.2g/out32/libeay32.lib")
+  "Dependencies/openssl-1.0.2g/out32/libeay32.lib")
 set(OPEN_SSL_BASE_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/openssl-1.0.2g/out32/libeay32.lib")
+  "Dependencies/openssl-1.0.2g/out32/libeay32.lib")
 set(OPEN_SSL_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/openssl-1.0.2g/out32/ssleay32.lib")
+  "Dependencies/openssl-1.0.2g/out32/ssleay32.lib")
 set(OPEN_SSL_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/openssl-1.0.2g/out32/ssleay32.lib")
-set(PYTHON_INCLUDE_PATH "${DEPENDENCIES_PATH}/Python-3.7.2/Include")
+  "Dependencies/openssl-1.0.2g/out32/ssleay32.lib")
+set(PYTHON_INCLUDE_PATH "Dependencies/Python-3.7.2/Include")
 set(PYTHON_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/Python-3.7.2/PCbuild/win32/python37.lib")
+  "Dependencies/Python-3.7.2/PCbuild/win32/python37.lib")
 set(PYTHON_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/Python-3.7.2/PCbuild/win32/python37.lib")
-set(SQLITE_INCLUDE_PATH "${DEPENDENCIES_PATH}/sqlite-amalgamation-3230100")
+  "Dependencies/Python-3.7.2/PCbuild/win32/python37.lib")
+set(SQLITE_INCLUDE_PATH "Dependencies/sqlite-amalgamation-3230100")
 set(SQLITE_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/sqlite-amalgamation-3230100/sqlite3d.lib")
+  "Dependencies/sqlite-amalgamation-3230100/sqlite3d.lib")
 set(SQLITE_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/sqlite-amalgamation-3230100/sqlite3.lib")
-set(TCLAP_INCLUDE_PATH "${DEPENDENCIES_PATH}/tclap-1.2.1/include")
-set(VIPER_INCLUDE_PATH "${DEPENDENCIES_PATH}/viper/Include")
-set(YAML_INCLUDE_PATH "${DEPENDENCIES_PATH}/yaml-cpp/include")
+  "Dependencies/sqlite-amalgamation-3230100/sqlite3.lib")
+set(TCLAP_INCLUDE_PATH "Dependencies/tclap-1.2.1/include")
+set(VIPER_INCLUDE_PATH "Dependencies/viper/Include")
+set(YAML_INCLUDE_PATH "Dependencies/yaml-cpp/include")
 set(YAML_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/yaml-cpp/build/Debug/libyaml-cppmdd.lib")
+  "Dependencies/yaml-cpp/build/Debug/libyaml-cppmdd.lib")
 set(YAML_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/yaml-cpp/build/Release/libyaml-cppmd.lib")
-set(ZLIB_INCLUDE_PATH "${DEPENDENCIES_PATH}/zlib-1.2.11")
+  "Dependencies/yaml-cpp/build/Release/libyaml-cppmd.lib")
+set(ZLIB_INCLUDE_PATH "Dependencies/zlib-1.2.11")
 set(ZLIB_LIBRARY_DEBUG_PATH
-  "${DEPENDENCIES_PATH}/zlib-1.2.11/contrib/vstudio/vc14/x86/ZlibStatDebug/zlibstat.lib")
+  "Dependencies/zlib-1.2.11/contrib/vstudio/vc14/x86/ZlibStatDebug/zlibstat.lib")
 set(ZLIB_LIBRARY_OPTIMIZED_PATH
-  "${DEPENDENCIES_PATH}/zlib-1.2.11/contrib/vstudio/vc14/x86/ZlibStatReleaseWithoutAsm/zlibstat.lib")
+  "Dependencies/zlib-1.2.11/contrib/vstudio/vc14/x86/ZlibStatReleaseWithoutAsm/zlibstat.lib")

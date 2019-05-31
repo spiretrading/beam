8ae2a62e9ceb4cc147c981026bece6f7320cb18e
Kamal <kamal@eidolonsystems.com>
2019-05-31 17:02:03 -0400

    Added support for representing special values of ptime in Python.

--- Beam/Source/Python/DateTime.cpp
+++ Beam/Source/Python/DateTime.cpp
@@ -14,14 +14,25 @@ using namespace std;
 namespace {
   struct PtimeToPython {
     static PyObject* convert(const ptime& value) {
-      auto day = value.date();
-      auto timeOfDay = value.time_of_day();
-      auto usec = static_cast<int>(timeOfDay.total_microseconds() % 1000000);
-      auto result = PyDateTime_FromDateAndTime(day.year(), day.month(),
-        day.day(), static_cast<int>(timeOfDay.hours()),
-        static_cast<int>(timeOfDay.minutes()),
-        static_cast<int>(timeOfDay.seconds()), usec);
-      return incref(result);
+      if(value == not_a_date_time) {
+        return Py_None;
+      } else if(value == neg_infin) {
+        auto result = PyDateTime_FromDateAndTime(1, 1, 1, 0, 0, 0, 0);
+        return incref(result);
+      } else if(value == pos_infin) {
+        auto result = PyDateTime_FromDateAndTime(9999, 12, 31, 23, 59, 59,
+          999999);
+        return incref(result);
+      } else {
+        auto day = value.date();
+        auto timeOfDay = value.time_of_day();
+        auto usec = static_cast<int>(timeOfDay.total_microseconds() % 1000000);
+        auto result = PyDateTime_FromDateAndTime(day.year(), day.month(),
+          day.day(), static_cast<int>(timeOfDay.hours()),
+          static_cast<int>(timeOfDay.minutes()),
+          static_cast<int>(timeOfDay.seconds()), usec);
+        return incref(result);
+      }
     }
   };
 
@@ -36,17 +47,28 @@ namespace {
     static void construct(PyObject* object,
         boost::python::converter::rvalue_from_python_stage1_data* data) {
       auto storage = reinterpret_cast<converter::rvalue_from_python_storage<
-          ptime>*>(data)->storage.bytes;
+        ptime>*>(data)->storage.bytes;
       if(object == Py_None) {
-        new(storage) ptime{not_a_date_time};
+        new(storage) ptime(not_a_date_time);
       } else {
-        date d(PyDateTime_GET_YEAR(object), PyDateTime_GET_MONTH(object),
+        auto d = date(PyDateTime_GET_YEAR(object), PyDateTime_GET_MONTH(object),
           PyDateTime_GET_DAY(object));
-        time_duration timeOfDay(PyDateTime_DATE_GET_HOUR(object),
+        auto timeOfDay = time_duration(PyDateTime_DATE_GET_HOUR(object),
           PyDateTime_DATE_GET_MINUTE(object),
           PyDateTime_DATE_GET_SECOND(object));
-        timeOfDay += microsec(PyDateTime_DATE_GET_MICROSECOND(object));
-        new(storage) ptime(d, timeOfDay);
+        auto us = microsec(PyDateTime_DATE_GET_MICROSECOND(object));
+        timeOfDay += us;
+        if(d.year() == 9999 && d.month() == 12 && d.day() == 31 &&
+            timeOfDay.hours() == 23 && timeOfDay.minutes() == 59 &&
+            timeOfDay.seconds() == 59 && us == microsec(999999)) {
+          new(storage) ptime(pos_infin);
+        } else if(d.year() == 1 && d.month() == 1 && d.day() == 1 &&
+            timeOfDay.hours() == 0 && timeOfDay.minutes() == 0 &&
+            timeOfDay.seconds() == 0 && us == microsec(0)) {
+          new(storage) ptime(neg_infin);
+        } else {
+          new(storage) ptime(d, timeOfDay);
+        }
       }
       data->convertible = storage;
     }
@@ -93,7 +115,7 @@ namespace {
       if(isNegative) {
         days = -days;
       }
-      time_duration duration = hours(24) * days + seconds(timeDelta->seconds) +
+      auto duration = hours(24) * days + seconds(timeDelta->seconds) +
         microseconds(timeDelta->microseconds);
       if(isNegative) {
         duration = duration.invert_sign();

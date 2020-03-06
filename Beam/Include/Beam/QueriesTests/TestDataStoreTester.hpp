99fa972cf954bce24ae468426d67eb5292b64a10
Kamal <kamal@eidolonsystems.com>
2020-03-06 10:31:01 -0500

    Added unit tests for TestDataStore.

--- Beam/Include/Beam/QueriesTests/TestDataStoreTester.hpp
+++ Beam/Include/Beam/QueriesTests/TestDataStoreTester.hpp
@@ -10,11 +10,28 @@ namespace Beam::Queries::Tests {
   class TestDataStoreTester : public CPPUNIT_NS::TestFixture {
     public:
 
-      /** Tests a Store operation. */
+      /** Tests an Open operation with exception. */
+      void TestOpenException();
+
+      /** Tests a Store operation without exception. */
       void TestStore();
+
+      /** Tests a Store operation with exception. */
+      void TestStoreException();
+
+      /** Tests a Load operation without exception. */
+      void TestLoad();
+
+      /** Tests a Load operation with exception. */
+      void TestLoadException();
 
     private:
       CPPUNIT_TEST_SUITE(TestDataStoreTester);
+        CPPUNIT_TEST(TestOpenException);
+        CPPUNIT_TEST(TestStore);
+        CPPUNIT_TEST(TestStoreException);
+        CPPUNIT_TEST(TestLoad);
+        CPPUNIT_TEST(TestLoadException);
         CPPUNIT_TEST(TestStore);
       BEAM_CPPUNIT_TEST_SUITE_END();
   };

7b366c7bcd18429565fb46e95717832b8baf21d8
Kamal <kamal@eidolonsystems.com>
2020-04-15 18:58:17 -0400

    Moved StompTests to doctest.

--- Beam/Source/StompTests/StompFrameParserTester.cpp
+++ Beam/Source/StompTests/StompFrameParserTester.cpp
@@ -1,109 +1,109 @@
-#include "Beam/StompTests/StompFrameParserTester.hpp"
+#include <doctest/doctest.h>
 #include "Beam/Stomp/StompFrameParser.hpp"
 
 using namespace Beam;
 using namespace Beam::Stomp;
-using namespace Beam::Stomp::Tests;
-using namespace std;
 
-void StompFrameParserTester::TestNewLineCharacter() {
-  StompFrameParser parser;
-  string contents =
-    "SEND\n"
-    "destination:/test/server\n"
-    "host:testhost\n"
-    "content-length:11\n\n"
-    "hello world\n";
-  parser.Feed(contents.c_str(), contents.size());
-  auto frame = parser.GetNextFrame();
-  CPPUNIT_ASSERT(frame.is_initialized());
-  CPPUNIT_ASSERT(frame->GetCommand() == StompCommand::SEND);
-  auto destinationHeader = frame->FindHeader("destination");
-  CPPUNIT_ASSERT(destinationHeader.is_initialized());
-  CPPUNIT_ASSERT(*destinationHeader == "/test/server");
-  auto hostHeader = frame->FindHeader("host");
-  CPPUNIT_ASSERT(hostHeader.is_initialized());
-  CPPUNIT_ASSERT(*hostHeader == "testhost");
-  auto body = frame->GetBody();
-  string message{body.GetData(), body.GetSize()};
-  CPPUNIT_ASSERT(message == "hello world");
-}
-
-void StompFrameParserTester::TestCarriageAndNewLineCharacter() {
-  StompFrameParser parser;
-  string contents =
-    "SEND\r\n"
-    "destination:/test/server\r\n"
-    "host:testhost\r\n"
-    "content-length:11\r\n\r\n"
-    "hello world\r\n";
-  parser.Feed(contents.c_str(), contents.size());
-  auto frame = parser.GetNextFrame();
-  CPPUNIT_ASSERT(frame.is_initialized());
-  CPPUNIT_ASSERT(frame->GetCommand() == StompCommand::SEND);
-  auto destinationHeader = frame->FindHeader("destination");
-  CPPUNIT_ASSERT(destinationHeader.is_initialized());
-  CPPUNIT_ASSERT(*destinationHeader == "/test/server");
-  auto hostHeader = frame->FindHeader("host");
-  CPPUNIT_ASSERT(hostHeader.is_initialized());
-  CPPUNIT_ASSERT(*hostHeader == "testhost");
-  auto body = frame->GetBody();
-  string message{body.GetData(), body.GetSize()};
-  CPPUNIT_ASSERT(message == "hello world");
-}
-
-void StompFrameParserTester::TestEolFrame() {
-  StompFrameParser parser;
-  string contents =
-    "\r\n\r\n\n\nSEND\r\n"
-    "destination:/test/server\r\n"
-    "host:testhost\r\n"
-    "content-length:11\r\n\r\n"
-    "hello world\r\n\r\n\n"
-    "SEND\r\n"
-    "destination:/dev/null\r\n\r\n"
-    "goodbye sky";
-  contents.push_back('\0');
-  contents += "\r\n";
-  parser.Feed(contents.c_str(), contents.size());
-  {
+TEST_SUITE("StompFrameParser") {
+  TEST_CASE("new_line_character") {
+    auto parser = StompFrameParser();
+    auto contents = std::string(
+      "SEND\n"
+      "destination:/test/server\n"
+      "host:testhost\n"
+      "content-length:11\n\n"
+      "hello world\n");
+    parser.Feed(contents.c_str(), contents.size());
     auto frame = parser.GetNextFrame();
-    CPPUNIT_ASSERT(frame.is_initialized());
-    CPPUNIT_ASSERT(frame->GetCommand() == StompCommand::SEND);
+    REQUIRE(frame.is_initialized());
+    REQUIRE(frame->GetCommand() == StompCommand::SEND);
     auto destinationHeader = frame->FindHeader("destination");
-    CPPUNIT_ASSERT(destinationHeader.is_initialized());
-    CPPUNIT_ASSERT(*destinationHeader == "/test/server");
+    REQUIRE(destinationHeader.is_initialized());
+    REQUIRE(*destinationHeader == "/test/server");
     auto hostHeader = frame->FindHeader("host");
-    CPPUNIT_ASSERT(hostHeader.is_initialized());
-    CPPUNIT_ASSERT(*hostHeader == "testhost");
+    REQUIRE(hostHeader.is_initialized());
+    REQUIRE(*hostHeader == "testhost");
     auto body = frame->GetBody();
-    string message{body.GetData(), body.GetSize()};
-    CPPUNIT_ASSERT(message == "hello world");
+    auto message = std::string(body.GetData(), body.GetSize());
+    REQUIRE(message == "hello world");
   }
-  {
-    auto frame = parser.GetNextFrame();
-    CPPUNIT_ASSERT(frame.is_initialized());
-    CPPUNIT_ASSERT(frame->GetCommand() == StompCommand::SEND);
-    auto destinationHeader = frame->FindHeader("destination");
-    CPPUNIT_ASSERT(destinationHeader.is_initialized());
-    CPPUNIT_ASSERT(*destinationHeader == "/dev/null");
-    auto body = frame->GetBody();
-    string message{body.GetData(), body.GetSize()};
-    CPPUNIT_ASSERT(message == "goodbye sky");
-  }
-}
 
-void StompFrameParserTester::TestEscapeCharacters() {
-  StompFrameParser parser;
-  string contents =
-    "SEND\r\n"
-    "destination:/test/server\r\n"
-    "a\\nb:a\\cb\r\n"
-    "content-length:0\r\n\r\n\r\n";
-  parser.Feed(contents.c_str(), contents.size());
-  auto frame = parser.GetNextFrame();
-  CPPUNIT_ASSERT(frame.is_initialized());
-  auto header = frame->FindHeader("a\nb");
-  CPPUNIT_ASSERT(header.is_initialized());
-  CPPUNIT_ASSERT(*header == "a:b");
+  TEST_CASE("carriage_and_new_line_character") {
+    auto parser = StompFrameParser();
+    auto contents = std::string(
+      "SEND\r\n"
+      "destination:/test/server\r\n"
+      "host:testhost\r\n"
+      "content-length:11\r\n\r\n"
+      "hello world\r\n");
+    parser.Feed(contents.c_str(), contents.size());
+    auto frame = parser.GetNextFrame();
+    REQUIRE(frame.is_initialized());
+    REQUIRE(frame->GetCommand() == StompCommand::SEND);
+    auto destinationHeader = frame->FindHeader("destination");
+    REQUIRE(destinationHeader.is_initialized());
+    REQUIRE(*destinationHeader == "/test/server");
+    auto hostHeader = frame->FindHeader("host");
+    REQUIRE(hostHeader.is_initialized());
+    REQUIRE(*hostHeader == "testhost");
+    auto body = frame->GetBody();
+    auto message = std::string(body.GetData(), body.GetSize());
+    REQUIRE(message == "hello world");
+  }
+
+  TEST_CASE("eol_frame") {
+    auto parser = StompFrameParser();
+    auto contents = std::string(
+      "\r\n\r\n\n\nSEND\r\n"
+      "destination:/test/server\r\n"
+      "host:testhost\r\n"
+      "content-length:11\r\n\r\n"
+      "hello world\r\n\r\n\n"
+      "SEND\r\n"
+      "destination:/dev/null\r\n\r\n"
+      "goodbye sky");
+    contents.push_back('\0');
+    contents += "\r\n";
+    parser.Feed(contents.c_str(), contents.size());
+    {
+      auto frame = parser.GetNextFrame();
+      REQUIRE(frame.is_initialized());
+      REQUIRE(frame->GetCommand() == StompCommand::SEND);
+      auto destinationHeader = frame->FindHeader("destination");
+      REQUIRE(destinationHeader.is_initialized());
+      REQUIRE(*destinationHeader == "/test/server");
+      auto hostHeader = frame->FindHeader("host");
+      REQUIRE(hostHeader.is_initialized());
+      REQUIRE(*hostHeader == "testhost");
+      auto body = frame->GetBody();
+      auto message = std::string(body.GetData(), body.GetSize());
+      REQUIRE(message == "hello world");
+    }
+    {
+      auto frame = parser.GetNextFrame();
+      REQUIRE(frame.is_initialized());
+      REQUIRE(frame->GetCommand() == StompCommand::SEND);
+      auto destinationHeader = frame->FindHeader("destination");
+      REQUIRE(destinationHeader.is_initialized());
+      REQUIRE(*destinationHeader == "/dev/null");
+      auto body = frame->GetBody();
+      auto message = std::string(body.GetData(), body.GetSize());
+      REQUIRE(message == "goodbye sky");
+    }
+  }
+
+  TEST_CASE("escape_characters") {
+    auto parser = StompFrameParser();
+    auto contents = std::string(
+      "SEND\r\n"
+      "destination:/test/server\r\n"
+      "a\\nb:a\\cb\r\n"
+      "content-length:0\r\n\r\n\r\n");
+    parser.Feed(contents.c_str(), contents.size());
+    auto frame = parser.GetNextFrame();
+    REQUIRE(frame.is_initialized());
+    auto header = frame->FindHeader("a\nb");
+    REQUIRE(header.is_initialized());
+    REQUIRE(*header == "a:b");
+  }
 }
--- Beam/Source/StompTests/StompServerTester.cpp
+++ Beam/Source/StompTests/StompServerTester.cpp
@@ -1,43 +1,53 @@
-#include "Beam/StompTests/StompServerTester.hpp"
+#include <doctest/doctest.h>
+#include "Beam/IO/LocalServerConnection.hpp"
+#include "Beam/IO/SharedBuffer.hpp"
+#include "Beam/Stomp/StompServer.hpp"
 
 using namespace Beam;
 using namespace Beam::IO;
+using namespace Beam::Routines;
 using namespace Beam::Stomp;
-using namespace Beam::Stomp::Tests;
 
-void StompServerTester::setUp() {
-  m_serverConnection.emplace();
-  m_serverConnection->Open();
-  m_clientChannel.emplace("stomp", Ref(*m_serverConnection));
-  Routines::Spawn(
-    [&] {
-      m_clientChannel->GetConnection().Open();
-    });
-  auto serverChannel = m_serverConnection->Accept();
-  m_server.emplace(std::move(serverChannel));
+namespace {
+  struct Fixture {
+    using ServerConnection = LocalServerConnection<SharedBuffer>;
+    using ServerChannel = LocalServerChannel<SharedBuffer>;
+    using ClientChannel = LocalClientChannel<SharedBuffer>;
+    using TestStompServer = StompServer<std::unique_ptr<ServerChannel>>;
+
+    ServerConnection m_serverConnection;
+    boost::optional<ClientChannel> m_clientChannel;
+    boost::optional<TestStompServer> m_server;
+
+    Fixture() {
+      m_serverConnection.Open();
+      m_clientChannel.emplace("stomp", Ref(m_serverConnection));
+      Spawn(
+        [&] {
+          m_clientChannel->GetConnection().Open();
+        });
+      m_server.emplace(m_serverConnection.Accept());
+    }
+  };
 }
 
-void StompServerTester::tearDown() {
-  m_server.reset();
-  m_clientChannel.reset();
-  m_serverConnection.reset();
-}
-
-void StompServerTester::TestReceivingConnectCommand() {
-  auto contents = BufferFromString<SharedBuffer>(
-    "CONNECT\n"
-    "accept-version:1.2\n"
-    "host:testhost\n\n\n");
-  m_clientChannel->GetWriter().Write(contents);
-  m_server->Open();
-  SharedBuffer responseBuffer;
-  m_clientChannel->GetReader().Read(Store(responseBuffer));
-  StompFrameParser parser;
-  parser.Feed(responseBuffer.GetData(), responseBuffer.GetSize());
-  auto response = parser.GetNextFrame();
-  CPPUNIT_ASSERT(response.is_initialized());
-  CPPUNIT_ASSERT(response->GetCommand() == StompCommand::CONNECTED);
-  auto versionHeader = response->FindHeader("version");
-  CPPUNIT_ASSERT(versionHeader.is_initialized());
-  CPPUNIT_ASSERT(*versionHeader == "1.2");
+TEST_SUITE("StompServer") {
+  TEST_CASE_FIXTURE(Fixture, "receiving_connect_command") {
+    auto contents = BufferFromString<SharedBuffer>(
+      "CONNECT\n"
+      "accept-version:1.2\n"
+      "host:testhost\n\n\n");
+    m_clientChannel->GetWriter().Write(contents);
+    m_server->Open();
+    auto responseBuffer = SharedBuffer();
+    m_clientChannel->GetReader().Read(Store(responseBuffer));
+    auto parser = StompFrameParser();
+    parser.Feed(responseBuffer.GetData(), responseBuffer.GetSize());
+    auto response = parser.GetNextFrame();
+    REQUIRE(response.is_initialized());
+    REQUIRE(response->GetCommand() == StompCommand::CONNECTED);
+    auto versionHeader = response->FindHeader("version");
+    REQUIRE(versionHeader.is_initialized());
+    REQUIRE(*versionHeader == "1.2");
+  }
 }

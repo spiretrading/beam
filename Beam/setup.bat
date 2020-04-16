54eadcf7a4d72648e31421289aab4ec469946c91
Kamal <kamal@eidolonsystems.com>
2020-04-16 16:36:10 -0400

    Changed setup script on Windows to check if wget results in exit code less than or equal to 0. For some reason wget sometimes returns a negative value on success.

--- Beam/setup.bat
+++ Beam/setup.bat
@@ -10,7 +10,7 @@ FOR /f "usebackq delims=" %%i IN (`!VSWHERE! -prerelease -latest -property insta
 )
 IF NOT EXIST Strawberry (
   wget http://strawberryperl.com/download/5.30.1.1/strawberry-perl-5.30.1.1-64bit-portable.zip -O strawberry-perl-5.30.1.1-64bit-portable.zip --no-check-certificate
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     MD Strawberry
     PUSHD Strawberry
     unzip ..\strawberry-perl-5.30.1.1-64bit-portable.zip
@@ -24,7 +24,7 @@ SET PATH=!PATH!;!ROOT!\Strawberry\perl\site\bin;!ROOT!\Strawberry\perl\bin;!ROOT
 SET BUILD_ASPEN=
 IF NOT EXIST aspen (
   git clone https://www.github.com/spiretrading/aspen
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     SET BUILD_ASPEN=1
   ) ELSE (
     RD /S /Q aspen
@@ -41,7 +41,7 @@ IF EXIST aspen (
     git checkout "!aspen_commit!"
     SET BUILD_ASPEN=1
   )
-  IF !BUILD_ASPEN! EQU 1 (
+  IF !BUILD_ASPEN! LEQ 1 (
     CALL configure.bat -DD="!ROOT!"
     CALL build.bat Debug
     CALL build.bat Release
@@ -54,7 +54,7 @@ IF EXIST aspen (
 )
 IF NOT EXIST cryptopp820 (
   wget https://www.cryptopp.com/cryptopp820.zip -O cryptopp820.zip --no-check-certificate
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     MD cryptopp820
     PUSHD cryptopp820
     unzip ..\cryptopp820.zip
@@ -77,7 +77,7 @@ IF NOT EXIST cryptopp820 (
 )
 IF NOT EXIST doctest-2.3.6 (
   wget https://github.com/onqtam/doctest/archive/2.3.6.zip --no-check-certificate
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     unzip 2.3.6.zip
   ) ELSE (
     SET EXIT_STATUS=1
@@ -86,7 +86,7 @@ IF NOT EXIST doctest-2.3.6 (
 )
 IF NOT EXIST mariadb-connector-c-3.0.6 (
   wget https://github.com/MariaDB/mariadb-connector-c/archive/v3.0.6.zip -O mariadb-connector-c-3.0.6.zip --no-check-certificate
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     unzip mariadb-connector-c-3.0.6.zip
     PUSHD mariadb-connector-c-3.0.6
     cmake -A Win32 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./mariadb .
@@ -112,7 +112,7 @@ IF NOT EXIST mariadb-connector-c-3.0.6 (
 )
 IF NOT EXIST openssl-1.1.1c (
   wget https://ftp.openssl.org/source/old/1.1.1/openssl-1.1.1c.tar.gz -O openssl-1.1.1c.tar.gz --no-check-certificate
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     gzip -d -c openssl-1.1.1c.tar.gz | tar -xf -
     MOVE openssl-1.1.1c openssl-1.1.1c-build
     PUSHD openssl-1.1.1c-build
@@ -129,7 +129,7 @@ IF NOT EXIST openssl-1.1.1c (
 )
 IF NOT EXIST sqlite-amalgamation-3300100 (
   wget https://www.sqlite.org/2019/sqlite-amalgamation-3300100.zip -O sqlite-amalgamation-3300100.zip --no-check-certificate
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     unzip sqlite-amalgamation-3300100.zip
     PUSHD sqlite-amalgamation-3300100
     cl /c /Zi /MDd /DSQLITE_USE_URI=1 sqlite3.c
@@ -146,7 +146,7 @@ IF NOT EXIST sqlite-amalgamation-3300100 (
 )
 IF NOT EXIST tclap-1.2.2 (
   wget https://github.com/mirror/tclap/archive/v1.2.2.zip -O v1.2.2.zip --no-check-certificate
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     unzip v1.2.2.zip
   ) ELSE (
     SET EXIT_STATUS=1
@@ -173,7 +173,7 @@ IF EXIST viper (
 )
 IF NOT EXIST yaml-cpp (
   git clone https://github.com/jbeder/yaml-cpp.git yaml-cpp
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     PUSHD yaml-cpp
     git checkout 0f9a586ca1dc29c2ecb8dd715a315b93e3f40f79
     MD build
@@ -190,7 +190,7 @@ IF NOT EXIST yaml-cpp (
 )
 IF NOT EXIST zlib-1.2.11 (
   git clone --branch v1.2.11 https://github.com/madler/zlib.git zlib-1.2.11
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     PUSHD zlib-1.2.11\contrib\vstudio\vc14
     TYPE zlibstat.vcxproj | sed "s/ZLIB_WINAPI;//" | sed "s/<RuntimeLibrary>MultiThreadedDebug<\/RuntimeLibrary>/<RuntimeLibrary>MultiThreadedDebugDLL<\/RuntimeLibrary>/" | sed "s/<RuntimeLibrary>MultiThreaded<\/RuntimeLibrary>/<RuntimeLibrary>MultiThreadedDLL<\/RuntimeLibrary>/" > zlibstat.vcxproj.new
     MOVE zlibstat.vcxproj.new zlibstat.vcxproj
@@ -209,7 +209,7 @@ IF "%NUMBER_OF_PROCESSORS%" == "" (
 )
 IF NOT EXIST boost_1_72_0 (
   wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.zip -O boost_1_72_0.zip --no-check-certificate
-  IF !ERRORLEVEL! EQU 0 (
+  IF !ERRORLEVEL! LEQ 0 (
     unzip boost_1_72_0.zip
     PUSHD boost_1_72_0
     PUSHD tools\build

09fa478312632b4e6de0c37652684aa5e18a9f10
Kamal <kamal@eidolonsystems.com>
2020-04-21 17:57:22 -0400

    Added documentation.

--- Beam/Include/Beam/Utilities/Casts.hpp
+++ Beam/Include/Beam/Utilities/Casts.hpp
@@ -5,14 +5,19 @@
 #include "Beam/Utilities/Utilities.hpp"
 
 namespace Beam {
+
+  //! Implements a callable object performing a static_cast.
   template<typename T>
   struct StaticCastConverter {
+
+    //! Performs a static_cast.
     template<typename U>
     constexpr decltype(auto) operator ()(U&& value) const {
       return static_cast<T>(std::forward<U>(value));
     }
   };
 
+  //! Performs a static_cast on a unique_ptr.
   template<typename T, typename U>
   T StaticCast(std::unique_ptr<U>&& source) {
     return T(static_cast<typename T::pointer>(source.release()));

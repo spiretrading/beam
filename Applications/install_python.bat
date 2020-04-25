2ba627d3e09f035af1e9a02f9f33dc41ae141e56
Kamal Mansouri <kamal@eidolonsystems.com>
2020-04-25 02:08:02 -0400

    Fixed how aspen's install script is called.

--- Applications/install_python.bat
+++ Applications/install_python.bat
@@ -1,10 +1,13 @@
+@ECHO OFF
 IF "%1" == "" (
   SET CONFIG=Release
 ) ELSE (
   SET CONFIG=%1
 )
 FOR /f "delims=" %%i IN ('python -m site --user-site') DO SET PYTHON_PATH="%%i"
-CALL ..\Beam\Dependencies\aspen\install_python.bat %*
+PUSHD ..\Beam\Dependencies\aspen
+CALL install_python.bat %*
+POPD
 MD %PYTHON_PATH%\beam
 COPY Python\__init__.py %PYTHON_PATH%\beam
 COPY ..\Beam\Libraries\%CONFIG%\_beam.pyd %PYTHON_PATH%\beam

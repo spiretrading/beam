d93f3c63c74bb611e17b80a1ce99ae2ffd78c4c2
Kamal Mansouri <kamal@eidolonsystems.com>
2020-04-26 20:52:20 -0400

    Fixed syntax error.

--- Applications/install_python.sh
+++ Applications/install_python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python_directory=$(python3 -m site --user-site)
 pushd ../Beam/Dependencies/aspen
-install_python.sh
+./install_python.sh
 popd
 mkdir -p $python_directory/beam
 cp Python/__init__.py $python_directory/beam

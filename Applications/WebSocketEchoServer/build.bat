81fcc59f98d29ec037ab46904d7e843bd33bf8ac
Kamal <kamal@eidolonsystems.com>
2020-05-29 18:21:59 -0400

    Updated build scripts to use native CMake.

--- Applications/WebSocketEchoServer/build.bat
+++ Applications/WebSocketEchoServer/build.bat
@@ -1,13 +1,41 @@
 @ECHO OFF
-SETLOCAL
-IF [%1] == [] (
-  SET config=Release
-) ELSE (
-  SET config="%1"
+SETLOCAL EnableDelayedExpansion
+SET ROOT=%cd%
+:begin_args
+SET ARG=%~1
+IF "!IS_DEPENDENCY!" == "1" (
+  SET DEPENDENCIES=!ARG!
+  SET IS_DEPENDENCY=
+  SHIFT
+  GOTO begin_args
+) ELSE IF NOT "!ARG!" == "" (
+  IF "!ARG:~0,3!" == "-DD" (
+    SET IS_DEPENDENCY=1
+  ) ELSE (
+    SET CONFIG=!ARG!
+  )
+  SHIFT
+  GOTO begin_args
 )
-IF "%1" == "clean" (
-  git clean -fxd -e *Dependencies*
+IF "!CONFIG!" == "" (
+  SET CONFIG=Release
+)
+IF "!CONFIG!" == "clean" (
+  git clean -ffxd -e *Dependencies*
+  IF EXIST Dependencies\last_check.txt (
+    DEL Dependencies\last_check.txt
+  )
+) ELSE IF "!CONFIG!" == "reset" (
+  git clean -ffxd
+  IF EXIST Dependencies\last_check.txt (
+    DEL Dependencies\last_check.txt
+  )
 ) ELSE (
-  cmake --build . --target INSTALL --config %config%
+  IF NOT "!DEPENDENCIES!" == "" (
+    cmake -S "!ROOT!" -DD=!DEPENDENCIES!
+  ) ELSE (
+    cmake -S "!ROOT!"
+  )
+  cmake --build "!ROOT!" --target INSTALL --config "!CONFIG!"
 )
 ENDLOCAL

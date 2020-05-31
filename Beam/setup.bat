4e7c4d5430dafdad943ae403191ce3907a3a4ad1
Kamal Mansouri <kamal@eidolonsystems.com>
2020-05-30 22:05:59 -0400

    Updated aspen.

--- Beam/setup.bat
+++ Beam/setup.bat
@@ -33,7 +33,7 @@ IF NOT EXIST Strawberry (
 )
 SET PATH=!PATH!;!ROOT!\Strawberry\perl\site\bin;!ROOT!\Strawberry\perl\bin;!ROOT!\Strawberry\c\bin
 SET BUILD_ASPEN=
-SET ASPEN_COMMIT="a8f5457c80c3b2f54636426c7eb9ee702eb396b7"
+SET ASPEN_COMMIT="572c6735780076b94ab860a923c5fe52eeaacff0"
 IF NOT EXIST aspen (
   git clone https://www.github.com/spiretrading/aspen
   IF !ERRORLEVEL! EQU 0 (
--- Beam/setup.sh
+++ Beam/setup.sh
@@ -21,7 +21,7 @@ if [ -f "cache_files/beam.txt" ]; then
   fi
 fi
 cores="`grep -c "processor" < /proc/cpuinfo`"
-aspen_commit="a8f5457c80c3b2f54636426c7eb9ee702eb396b7"
+aspen_commit="572c6735780076b94ab860a923c5fe52eeaacff0"
 build_aspen=0
 if [ ! -d "aspen" ]; then
   git clone https://www.github.com/spiretrading/aspen

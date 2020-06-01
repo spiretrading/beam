c798d4089fd5626a18bac31c34f1eb75a2196b4b
Kamal <kamal@eidolonsystems.com>
2020-06-01 16:59:37 -0400

    Added missing WebApi target to build script.

--- build.sh
+++ build.sh
@@ -7,6 +7,12 @@ while [ -h "$source" ]; do
 done
 directory="$(cd -P "$(dirname "$source")" >/dev/null 2>&1 && pwd)"
 root=$(pwd)
+if [ ! -f "configure.sh" ]; then
+  ln -s "$directory/configure.sh" configure.sh
+fi
+if [ ! -f "build.sh" ]; then
+  ln -s "$directory/build.sh" build.sh
+fi
 build_function() {
   location="${@: -1}"
   if [ ! -d "$location" ]; then
@@ -23,7 +29,7 @@ export root
 
 build_function "$@" "Beam"
 targets="WebApi"
-targets="Applications/AdminClient"
+targets+=" Applications/AdminClient"
 targets+=" Applications/ClientTemplate"
 targets+=" Applications/DataStoreProfiler"
 targets+=" Applications/HttpFileServer"

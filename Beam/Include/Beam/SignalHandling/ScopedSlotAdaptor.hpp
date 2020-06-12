aade04dd33029a13652b78e8194c00e3be1ea1ce
Kamal <kamal@eidolonsystems.com>
2020-06-12 12:03:39 -0400

    ScopedSlotAdaptor will throw an exception on expiry instead of returning a default value.

--- Beam/Include/Beam/SignalHandling/ScopedSlotAdaptor.hpp
+++ Beam/Include/Beam/SignalHandling/ScopedSlotAdaptor.hpp
@@ -1,127 +1,81 @@
-#ifndef BEAM_SCOPEDSLOTADAPTOR_HPP
-#define BEAM_SCOPEDSLOTADAPTOR_HPP
-#include <functional>
+#ifndef BEAM_SCOPED_SLOT_ADAPTOR_HPP
+#define BEAM_SCOPED_SLOT_ADAPTOR_HPP
+#include <memory>
+#include <stdexcept>
+#include <type_traits>
 #include <utility>
 #include <boost/noncopyable.hpp>
 #include "Beam/SignalHandling/SignalHandling.hpp"
 #include "Beam/Threading/RecursiveMutex.hpp"
 #include "Beam/Threading/Sync.hpp"
-#include "Beam/Utilities/Functional.hpp"
 
-namespace Beam {
-namespace SignalHandling {
+namespace Beam::SignalHandling {
 namespace Details {
-  template<typename F, typename T>
-  struct Callback {
-    std::shared_ptr<Threading::Sync<bool, Threading::RecursiveMutex>>
-      m_keepAlive;
-    F m_callback;
-    T m_default;
-
-    template<typename CallbackForward>
-    Callback(std::shared_ptr<
-        Threading::Sync<bool, Threading::RecursiveMutex>> keepAlive,
-        CallbackForward&& callback, const T& defaultValue)
-        : m_keepAlive(std::move(keepAlive)),
-          m_callback(std::forward<CallbackForward>(callback)),
-          m_default(defaultValue) {}
-
-    template<typename... Args>
-    T operator ()(Args&&... args) {
-      return Threading::With(*m_keepAlive,
-        [&] (bool keepAlive) {
-          if(keepAlive) {
-            return m_callback(std::forward<Args>(args)...);
-          } else {
-            return m_default;
-          }
-        });
-    }
-  };
-
   template<typename F>
-  struct Callback<F, void> {
-    std::shared_ptr<Threading::Sync<bool, Threading::RecursiveMutex>>
-      m_keepAlive;
-    F m_callback;
+  struct ScopedSlot {
+    std::shared_ptr<Threading::Sync<bool, Threading::RecursiveMutex>> m_isAlive;
+    F m_slot;
 
-    template<typename CallbackForward>
-    Callback(std::shared_ptr<
-        Threading::Sync<bool, Threading::RecursiveMutex>> keepAlive,
-        CallbackForward&& callback)
-        : m_keepAlive(std::move(keepAlive)),
-          m_callback(std::forward<CallbackForward>(callback)) {}
+    template<typename SlotForward>
+    ScopedSlot(std::shared_ptr<
+      Threading::Sync<bool, Threading::RecursiveMutex>> keepAlive,
+      SlotForward&& slot)
+      : m_isAlive(std::move(keepAlive)),
+        m_slot(std::forward<SlotForward>(slot)) {}
 
     template<typename... Args>
     void operator ()(Args&&... args) {
-      return Threading::With(*m_keepAlive,
-        [&] (bool keepAlive) {
-          if(keepAlive) {
-            m_callback(std::forward<Args>(args)...);
+      Threading::With(*m_isAlive,
+        [&] (bool isAlive) {
+          if(isAlive) {
+            m_slot(std::forward<Args>(args)...);
+          } else {
+            throw std::runtime_error("Slot expired.");
           }
         });
     }
   };
 }
 
-  /*! \class ScopedSlotAdaptor
-      \brief Produces callbacks that only get invoked if the object that
-             constructed it is in scope.
+  /**
+   * Produces slots that only get invoked if the object that constructed it
+   * is in scope.
    */
   class ScopedSlotAdaptor : private boost::noncopyable {
     public:
 
-      //! Constructs a ScopedSlotAdaptor.
+      /** Constructs a ScopedSlotAdaptor. */
       ScopedSlotAdaptor();
 
       ~ScopedSlotAdaptor();
 
-      //! Returns a callback whose scope is tied to this object.
-      /*!
-        \param callback The callback to scope.
-      */
+      /**
+       * Returns a slot whose scope is tied to this object.
+       *
+       * @param slot The slot to scopt.
+       */
       template<typename F>
-      Details::Callback<typename std::decay<F>::type, void> GetCallback(
-        F&& callback) const;
-
-      //! Returns a callback whose scope is tied to this object.
-      /*!
-        \param callback The callback to scope.
-        \param defaultValue The value to return if the callback is not in scope.
-      */
-      template<typename F, typename T>
-      Details::Callback<typename std::decay<F>::type,
-        typename std::decay<T>::type> GetCallback(F&& callback,
-        T&& defaultValue) const;
+      Details::ScopedSlot<std::decay_t<F>> MakeSlot(F&& slot) const;
 
     private:
       std::shared_ptr<Threading::Sync<bool, Threading::RecursiveMutex>>
-        m_keepAlive;
+        m_isAlive;
   };
 
   inline ScopedSlotAdaptor::ScopedSlotAdaptor()
-      : m_keepAlive(std::make_shared<
-          Threading::Sync<bool, Threading::RecursiveMutex>>(true)) {}
+    : m_isAlive(std::make_shared<
+        Threading::Sync<bool, Threading::RecursiveMutex>>(true)) {}
 
   inline ScopedSlotAdaptor::~ScopedSlotAdaptor() {
-    *m_keepAlive = false;
+    *m_isAlive = false;
   }
 
   template<typename F>
-  Details::Callback<typename std::decay<F>::type, void>
-      ScopedSlotAdaptor::GetCallback(F&& callback) const {
-    return Details::Callback<typename std::decay<F>::type, void>(m_keepAlive,
-      std::forward<F>(callback));
+  Details::ScopedSlot<std::decay_t<F>> ScopedSlotAdaptor::MakeSlot(
+      F&& slot) const {
+    return Details::ScopedSlot<std::decay_t<F>>(m_isAlive,
+      std::forward<F>(slot));
   }
-
-  template<typename F, typename T>
-  Details::Callback<typename std::decay<F>::type, typename std::decay<T>::type>
-      ScopedSlotAdaptor::GetCallback(F&& callback, T&& defaultValue) const {
-    return Details::Callback<typename std::decay<F>::type,
-      typename std::decay<T>::type>(m_keepAlive, std::forward<F>(callback),
-      std::forward<T>(defaultValue));
-  }
-}
 }
 
 #endif

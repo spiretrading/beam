1f817628ddd7ad91ad88df5bc0c5e8d6d1f98bf6
Kamal Mansouri <kamal@eidolonsystems.com>
2020-07-30 01:56:54 -0400

    Updated the coding style for TablePublisher.

--- Beam/Include/Beam/Queues/TablePublisher.hpp
+++ Beam/Include/Beam/Queues/TablePublisher.hpp
@@ -1,223 +1,187 @@
-#ifndef BEAM_TABLEPUBLISHER_HPP
-#define BEAM_TABLEPUBLISHER_HPP
+#ifndef BEAM_TABLE_PUBLISHER_HPP
+#define BEAM_TABLE_PUBLISHER_HPP
 #include <unordered_map>
 #include "Beam/Queues/MultiQueueWriter.hpp"
 #include "Beam/Queues/Publisher.hpp"
 #include "Beam/Queues/Queues.hpp"
 #include "Beam/Queues/QueueWriter.hpp"
 #include "Beam/Queues/SnapshotPublisher.hpp"
+#include "Beam/Serialization/DataShuttle.hpp"
 #include "Beam/Threading/RecursiveMutex.hpp"
 
 namespace Beam {
 
-  /*! \class TableEntry
-      \brief Represents a single entry in a table.
-      \tparam KeyType The unique index/key into the table.
-      \tparam ValueType The value associated with the key.
+  /**
+   * Represents a single entry in a table.
+   * @param <K> The unique index/key into the table.
+   * @param <V> The value associated with the key.
    */
-  template<typename KeyType, typename ValueType>
+  template<typename K, typename V>
   struct TableEntry {
 
-    //! The unique index/key into the table.
-    using Key = KeyType;
+    /** The unique index/key into the table. */
+    using Key = K;
 
-    //! The value associated with the key.
-    using Value = ValueType;
+    /** The value associated with the key. */
+    using Value = V;
 
-    //! The unique index/key into the table.
+    /** The unique index/key into the table. */
     Key m_key;
 
-    //! The value associated with the key.
+    /** The value associated with the key. */
     Value m_value;
-
-    //! Constructs a TableEntry.
-    TableEntry() = default;
-
-    //! Constructs a TableEntry.
-    /*!
-      \param key The table entry's key.
-      \param value The value to associate with the <i>key</i>.
-    */
-    TableEntry(const Key& key, const Value& value);
   };
 
-  //! Builds a TableEntry.
-  /*!
-    \param key The table entry's key.
-    \param value The value to associate with the <i>key</i>.
-  */
-  template<typename Key, typename Value>
-  TableEntry<Key, Value> MakeTableEntry(const Key& key, const Value& value) {
-    return TableEntry<Key, Value>(key, value);
-  }
-
-  /*! \class TablePublisher
-      \brief Publishes updates to a table.
-      \tparam KeyType The unique index/key into the table.
-      \tparam ValueType The value associated with the key.
+  /**
+   * Publishes updates to a table.
+   * @param <K> The unique index/key into the table.
+   * @param <V> The value associated with the key.
    */
-  template<typename KeyType, typename ValueType>
-  class TablePublisher : public SnapshotPublisher<
-      TableEntry<KeyType, ValueType>, std::unordered_map<KeyType, ValueType>>,
-      public QueueWriter<TableEntry<KeyType, ValueType>> {
+  template<typename K, typename V>
+  class TablePublisher final :
+      public SnapshotPublisher<TableEntry<K, V>, std::unordered_map<K, V>>,
+      public QueueWriter<TableEntry<K, V>> {
     public:
-      using Type = typename Publisher<TableEntry<KeyType, ValueType>>::Type;
-      using Snapshot = typename SnapshotPublisher<TableEntry<
-        KeyType, ValueType>, std::unordered_map<KeyType, ValueType>>::Snapshot;
+      using Type = typename Publisher<TableEntry<K, V>>::Type;
+      using Snapshot = typename SnapshotPublisher<TableEntry<K, V>,
+        std::unordered_map<K, V>>::Snapshot;
 
-      //! The unique index/key into the table.
-      using Key = KeyType;
+      /** The unique index/key into the table. */
+      using Key = K;
 
-      //! The value associated with the key.
-      using Value = ValueType;
+      /** The value associated with the key. */
+      using Value = V;
 
-      //! Constructs a TablePublisher.
+      /** Constructs a TablePublisher. */
       TablePublisher() = default;
 
-      virtual ~TablePublisher() override final;
-
-      //! Pushes a key/value pair onto the table.
-      /*!
-        \param key The table entry's key.
-        \param value The value to associate with the <i>key</i>.
-      */
+      /**
+       * Pushes a key/value pair onto the table.
+       * @param key The table entry's key.
+       * @param value The value to associate with the <i>key</i>.
+       */
       void Push(const Key& key, const Value& value);
 
-      //! Deletes a key/value pair from the table.
-      /*!
-        \param key The key to delete.
-        \param value The value to publish indicating the value is being deleted.
-      */
+      /**
+       * Deletes a key/value pair from the table.
+       * @param key The key to delete.
+       * @param value The value to publish indicating the value is being
+       *        deleted.
+       */
       void Delete(const Key& key, const Value& value);
 
-      //! Deletes a key/value pair from the table.
-      /*!
-        \param key The key to delete.
-        \param value The value to publish indicating the value is being deleted.
-      */
+      /**
+       * Deletes a key/value pair from the table.
+       * @param key The key to delete.
+       * @param value The value to publish indicating the value is being
+       *        deleted.
+       */
       void Delete(const Type& value);
 
-      virtual void WithSnapshot(const std::function<
-        void (boost::optional<const Snapshot&>)>& f) const override final;
+      void WithSnapshot(const std::function<
+        void (boost::optional<const Snapshot&>)>& f) const override;
 
-      virtual void Monitor(std::shared_ptr<QueueWriter<Type>> queue,
-        Out<boost::optional<Snapshot>> snapshot) const override final;
+      void Monitor(std::shared_ptr<QueueWriter<Type>> queue,
+        Out<boost::optional<Snapshot>> snapshot) const override;
 
-      virtual void With(const std::function<void ()>& f) const override final;
+      void With(const std::function<void ()>& f) const override;
 
-      virtual void Monitor(
-        std::shared_ptr<QueueWriter<Type>> monitor) const override final;
+      void Monitor(std::shared_ptr<QueueWriter<Type>> monitor) const override;
 
-      virtual void Push(const Type& value) override final;
+      void Push(const Type& value) override;
 
-      virtual void Push(Type&& value) override final;
+      void Push(Type&& value) override;
 
-      virtual void Break(const std::exception_ptr& e) override final;
+      void Break(const std::exception_ptr& e) override;
 
-      using QueueWriter<TableEntry<KeyType, ValueType>>::Break;
+      using QueueWriter<TableEntry<Key, Value>>::Break;
     private:
       mutable Threading::RecursiveMutex m_mutex;
       std::unordered_map<Key, Value> m_table;
       MultiQueueWriter<Type> m_queue;
   };
 
-  template<typename KeyType, typename ValueType>
-  TableEntry<KeyType, ValueType>::TableEntry(const Key& key, const Value& value)
-      : m_key(key),
-        m_value(value) {}
-
-  template<typename KeyType, typename ValueType>
-  TablePublisher<KeyType, ValueType>::~TablePublisher() {
-    Break();
+  template<typename K, typename V>
+  void TablePublisher<K, V>::Push(const Key& key, const Value& value) {
+    Push(Type{key, value});
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::Push(const Key& key,
-      const Value& value) {
-    Push(Type(key, value));
-  }
-
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::Delete(const Key& key,
-      const Value& value) {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+  template<typename K, typename V>
+  void TablePublisher<K, V>::Delete(const Key& key, const Value& value) {
+    auto lock = boost::lock_guard(m_mutex);
     m_table.erase(key);
-    m_queue.Push(Type(key, value));
+    m_queue.Push(Type{key, value});
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::Delete(const Type& value) {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+  template<typename K, typename V>
+  void TablePublisher<K, V>::Delete(const Type& value) {
+    auto lock = boost::lock_guard(m_mutex);
     m_table.erase(value.m_key);
     m_queue.Push(value);
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::WithSnapshot(
+  template<typename K, typename V>
+  void TablePublisher<K, V>::WithSnapshot(
       const std::function<void (boost::optional<const Snapshot&>)>& f) const {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+    auto lock = boost::lock_guard(m_mutex);
     f(m_table);
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::Monitor(
-      std::shared_ptr<QueueWriter<Type>> queue,
+  template<typename K, typename V>
+  void TablePublisher<K, V>::Monitor(std::shared_ptr<QueueWriter<Type>> queue,
       Out<boost::optional<Snapshot>> snapshot) const {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+    auto lock = boost::lock_guard(m_mutex);
     *snapshot = m_table;
     m_queue.Monitor(queue);
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::With(
-      const std::function<void ()>& f) const {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+  template<typename K, typename V>
+  void TablePublisher<K, V>::With(const std::function<void ()>& f) const {
+    auto lock = boost::lock_guard(m_mutex);
     f();
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::Monitor(
+  template<typename K, typename V>
+  void TablePublisher<K, V>::Monitor(
       std::shared_ptr<QueueWriter<Type>> queue) const {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+    auto lock = boost::lock_guard(m_mutex);
     for(auto& i : m_table) {
-      queue->Push(Type(i.first, i.second));
+      queue->Push(Type{i.first, i.second});
     }
     m_queue.Monitor(queue);
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::Push(const Type& value) {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+  template<typename K, typename V>
+  void TablePublisher<K, V>::Push(const Type& value) {
+    auto lock = boost::lock_guard(m_mutex);
     m_table[value.m_key] = value.m_value;
     m_queue.Push(value);
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::Push(Type&& value) {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+  template<typename K, typename V>
+  void TablePublisher<K, V>::Push(Type&& value) {
+    auto lock = boost::lock_guard(m_mutex);
     m_table[value.m_key] = value.m_value;
     m_queue.Push(std::move(value));
   }
 
-  template<typename KeyType, typename ValueType>
-  void TablePublisher<KeyType, ValueType>::Break(const std::exception_ptr& e) {
-    boost::lock_guard<Threading::RecursiveMutex> lock(m_mutex);
+  template<typename K, typename V>
+  void TablePublisher<K, V>::Break(const std::exception_ptr& e) {
+    auto lock = boost::lock_guard(m_mutex);
     m_queue.Break(e);
   }
 }
 
-namespace Beam {
-namespace Serialization {
-  template<typename KeyType, typename ValueType>
-  struct Shuttle<TableEntry<KeyType, ValueType>> {
+namespace Beam::Serialization {
+  template<typename K, typename V>
+  struct Shuttle<TableEntry<K, V>> {
     template<typename Shuttler>
-    void operator ()(Shuttler& shuttle, TableEntry<KeyType, ValueType>& value,
+    void operator ()(Shuttler& shuttle, TableEntry<K, V>& value,
         unsigned int version) const {
       shuttle.Shuttle("key", value.m_key);
       shuttle.Shuttle("value", value.m_value);
     }
   };
-}
 }
 
 #endif

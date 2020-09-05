1fe20ac04edbd72cee0d65c33a14a3f4915fbec9
Kamal Mansouri <kamal@eidolonsystems.com>
2020-09-05 01:50:21 -0400

    AsyncWriter will catch any exception thrown when writing to its destination and report it on future writes.

--- Beam/Include/Beam/IO/AsyncWriter.hpp
+++ Beam/Include/Beam/IO/AsyncWriter.hpp
@@ -1,5 +1,5 @@
-#ifndef BEAM_ASYNCWRITER_HPP
-#define BEAM_ASYNCWRITER_HPP
+#ifndef BEAM_ASYNC_WRITER_HPP
+#define BEAM_ASYNC_WRITER_HPP
 #include "Beam/IO/SharedBuffer.hpp"
 #include "Beam/IO/Writer.hpp"
 #include "Beam/Pointers/Dereference.hpp"
@@ -9,25 +9,24 @@
 namespace Beam {
 namespace IO {
 
-  /*! \class AsyncWriter
-      \brief Asynchronously writes to a destination using a Routine.
-      \tparam DestinationWriterType The Writer to write to.
+  /**
+   * Asynchronously writes to a destination using a Routine.
+   * @param <W> The Writer to write to.
    */
-  template<typename DestinationWriterType>
-  class AsyncWriter : private boost::noncopyable {
+  template<typename W>
+  class AsyncWriter {
     public:
       using Buffer = SharedBuffer;
 
-      //! The destination to write to.
-      using DestinationWriter =
-        typename TryDereferenceType<DestinationWriterType>::type;
+      /** The destination to write to. */
+      using DestinationWriter = GetTryDereferenceType<W>;
 
-      //! Constructs an AsyncWriter.
-      /*!
-        \param destination Used to initialize the destination of all writes.
-      */
-      template<typename DestinationWriterForward>
-      AsyncWriter(DestinationWriterForward&& destination);
+      /**
+       * Constructs an AsyncWriter.
+       * @param destination Used to initialize the destination of all writes.
+       */
+      template<typename WF>
+      AsyncWriter(WF&& destination);
 
       void Write(const void* data, std::size_t size);
 
@@ -37,42 +36,49 @@ namespace IO {
       void Write(const BufferType& data);
 
     private:
-      typename OptionalLocalPtr<DestinationWriterType>::type m_destination;
+      GetOptionalLocalPtr<W> m_destination;
+      std::exception_ptr m_exception;
       RoutineTaskQueue m_tasks;
   };
 
-  template<typename DestinationWriterType>
-  template<typename DestinationWriterForward>
-  AsyncWriter<DestinationWriterType>::AsyncWriter(
-      DestinationWriterForward&& destination)
-      : m_destination(std::forward<DestinationWriterForward>(destination)) {}
+  template<typename W>
+  template<typename WF>
+  AsyncWriter<W>::AsyncWriter(WF&& destination)
+    : m_destination(std::forward<WF>(destination)) {}
 
-  template<typename DestinationWriterType>
-  void AsyncWriter<DestinationWriterType>::Write(const void* data,
-      std::size_t size) {
-    SharedBuffer buffer(data, size);
-    Write(buffer);
+  template<typename W>
+  void AsyncWriter<W>::Write(const void* data, std::size_t size) {
+    Write(SharedBuffer(data, size));
   }
 
-  template<typename DestinationWriterType>
-  void AsyncWriter<DestinationWriterType>::Write(const SharedBuffer& data) {
-    m_tasks.Push(
-      [=] {
-        m_destination->Write(data);
+  template<typename W>
+  void AsyncWriter<W>::Write(const SharedBuffer& data) {
+    try {
+      m_tasks.Push([=] {
+        try {
+          m_destination->Write(data);
+        } catch(const std::exception&) {
+          if(!m_exception) {
+            m_exception = std::current_exception();
+            m_tasks.Break();
+          }
+        }
       });
+    } catch(const PipeBrokenException&) {
+      std::rethrow_exception(m_exception);
+    }
   }
 
-  template<typename DestinationWriterType>
+  template<typename W>
   template<typename BufferType>
-  void AsyncWriter<DestinationWriterType>::Write(const BufferType& data) {
-    SharedBuffer buffer = data;
-    Write(buffer);
+  void AsyncWriter<W>::Write(const BufferType& data) {
+    Write(data);
   }
 }
 
-  template<typename BufferType, typename DestinationWriterType>
-  struct ImplementsConcept<IO::AsyncWriter<DestinationWriterType>,
-    IO::Writer<BufferType>> : std::true_type {};
+  template<typename BufferType, typename W>
+  struct ImplementsConcept<IO::AsyncWriter<W>, IO::Writer<BufferType>> :
+    std::true_type {};
 }
 
 #endif

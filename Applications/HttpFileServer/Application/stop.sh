06300d2566c707b8e5ae55ec885632dc2332036e
Kamal <kamal@eidolonsystems.com>
2024-11-04 16:59:04 -0500

    Improved code styling of stop.sh.

--- Applications/HttpFileServer/Application/stop.sh
+++ Applications/HttpFileServer/Application/stop.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 APPLICATION="HttpFileServer"
-CHECK_SCRIPT="./check.sh"
 PID_FILE="pid.lock"
-TIMEOUT="300s"
-
-is_application_running() {
-  $CHECK_SCRIPT > /dev/null
-  return $?
-}
 
 if [[ -f "$PID_FILE" ]]; then
   pid=$(<"$PID_FILE")
-  if is_application_running > /dev/null; then
+  if ./check.sh > /dev/null; then
     kill -SIGINT "$pid" 2> /dev/null
-    timeout "$TIMEOUT" wait "$pid" 2> /dev/null || true
-    if is_application_running > /dev/null; then
-      kill -SIGKILL "$pid"
+    timeout 300s wait "$pid" 2> /dev/null
+    if [[ $? -eq 124 ]]; then
+      kill -SIGKILL "$pid" > /dev/null
       log_file=$(ls -t srv_*.log 2>/dev/null | head -n 1)
       if [[ -n "$log_file" ]]; then
        echo "Forcefully terminated $APPLICATION." >> "$log_file"

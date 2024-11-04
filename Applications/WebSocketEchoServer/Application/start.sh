f63612b40c03e1df3f878aa1188fa855ff19457b
Kamal <kamal@eidolonsystems.com>
2024-11-04 16:18:55 -0500

    Optimized the ops scripts and fixed a bug where multiple starts would move the server log file.

--- Applications/WebSocketEchoServer/Application/start.sh
+++ Applications/WebSocketEchoServer/Application/start.sh
@@ -2,6 +2,9 @@
 APPLICATION="WebSocketEchoServer"
 CONFIG_FILE="config.yml"
 LOG_DIR="./logs"
+if ./check.sh > /dev/null; then
+  exit 0
+fi
 mkdir -p "$LOG_DIR"
 date_time=$(date '+%Y%m%d_%H_%M_%S')
 log_name="srv_$date_time.log"
@@ -10,17 +13,12 @@ for existing_log in srv_*.log; do
     mv "$existing_log" "$LOG_DIR"
   fi
 done
-if ! (./check.sh | grep -q "$APPLICATION is not running."); then
-  exit 0
-fi
 ./$APPLICATION > "$log_name" 2>&1 &
 new_pid=$!
 echo "$new_pid" > "pid.lock"
-interface=$(yq '.interface // empty' "$CONFIG_FILE")
-all_addresses=()
-if [[ -n "$interface" ]]; then
-  all_addresses+=("$interface")
-fi
+all_addresses=($(yq -r \
+  '.interface? | select(. != null), .addresses[]? | select(. != null) | @sh' \
+  "$CONFIG_FILE"))
 if [[ ${#all_addresses[@]} -eq 0 ]]; then
   exit 0
 fi
@@ -37,7 +35,7 @@ is_any_address_listening() {
 }
 
 while true; do
-  if ./check.sh | grep -q "$APPLICATION is not running."; then
+  if ! ./check.sh > /dev/null; then
     exit 1
   fi
   if is_any_address_listening; then
